beb4a77ccb0c8bdcc6564d442b0d08418827cf86
xogur <snsk656@naver.com>
2025-08-25 19:28:03 +0900

    8 25 2

--- src/main/java/com/jpabook/jpashop/service/MemberService.java
+++ src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -44,4 +44,9 @@ public class MemberService {
     }
 
 
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne (id);
+        member.setName (name);
+    }
 }
