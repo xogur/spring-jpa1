cca5e80f12bff29733b2e434e8035180710a7f8f
xogur <snsk656@naver.com>
2025-08-31 14:22:10 +0900

    8 31 2

--- src/main/java/com/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/com/jpabook/jpashop/api/OrderApiController.java
@@ -1,4 +1,33 @@
 package com.jpabook.jpashop.api;
 
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAll (new OrderSearch ());
+        for (Order order : all) {
+            order.getMember ().getName ();
+            order.getDelivery ().getAddress ();
+            List<OrderItem> orderItems = order.getOrderItems ();
+            orderItems.stream ().forEach (o -> o.getItem ().getName ());
+        }
+        return all;
+    }
+
+
 }
